692684a3e847378cd76f312e91d3b24c5e7893e3
Jassica <jassica_fernandes@hotmail.com>
2020-06-21 02:51:50 +0530

    the final class

---
+++ src/test/java/cucumber/options/DemoGit/mergeClass.java
@@ -0,0 +1,15 @@
+package cucumber.options.DemoGit;
+
+public class mergeClass {
+	public void ajkjk()
+	{
+		System.out.println("ddvfvfvf");
+		System.out.println("fsvsvf");
+		System.out.println("new develop branch");
+		System.out.println("new develop branch1");
+		System.out.println("new develop branch2");
+		System.out.println("new develop branch3");
+		
+	}
+
+}
